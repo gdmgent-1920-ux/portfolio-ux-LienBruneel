674d07e094ca4fb62d46f92f995b7e2f74416875
LienBruneel <LienBruneel@users.noreply.github.com>
2020-02-29 16:03:24 +0100

    xd files
    XD file + XD link

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/05b7e8c7-2fd5-4799-7968-7c9cfd9c7c3f-7386/",
                 "target": "_blank"
             }
         ]
